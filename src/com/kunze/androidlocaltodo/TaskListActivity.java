e7bf8b7e8740f478b5897732e0dd31c228182418
boiler96 <boilerpdx@gmail.com>
2013-08-04 21:50:53 -0700

    tabs to spaces

--- src/com/kunze/androidlocaltodo/TaskListActivity.java
+++ src/com/kunze/androidlocaltodo/TaskListActivity.java
@@ -38,202 +38,202 @@ public class TaskListActivity extends Activity {
         getMenuInflater().inflate(R.menu.task_list, menu);
         return true;
     }
-    
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
-    	if (item.getItemId() == R.id.action_load_astrid)
-    	{
-    		LoadAstrid();
-    	}
-    	else if(item.getItemId() == R.id.action_delete_database)
-    	{
-    		DeleteDatabase();
-    	}
-		return true;
+        if (item.getItemId() == R.id.action_load_astrid)
+        {
+            LoadAstrid();
+        }
+        else if(item.getItemId() == R.id.action_delete_database)
+        {
+            DeleteDatabase();
+        }
+        return true;
     }
-    
+
     private void RefreshView()
     {
         // Get a cursor to populate the UI
-		Cursor cursor = mDB.GetCursor();
-		mAdapter.changeCursor(cursor);
-	}
-    
+        Cursor cursor = mDB.GetCursor();
+        mAdapter.changeCursor(cursor);
+    }
+
     private void InitializeView()
     {
         // Get a cursor to populate the UI
-		Cursor cursor = mDB.GetCursor();
-		String from[] = { "NAME" };
-		int to[] = { android.R.id.text1 };
-		
-		mAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, cursor, from, to, CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
-		ListView listView = (ListView)findViewById(R.id.task_list);
-		listView.setAdapter(mAdapter);
+        Cursor cursor = mDB.GetCursor();
+        String from[] = { "NAME" };
+        int to[] = { android.R.id.text1 };
+
+        mAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, cursor, from, to, CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+        ListView listView = (ListView)findViewById(R.id.task_list);
+        listView.setAdapter(mAdapter);
     }
-    
+
     private void DeleteDatabase()
     {
-    	AlertDialog.Builder builder = new AlertDialog.Builder(this);
-    	builder.setTitle("WARNING!");
-    	builder.setMessage("This will delete all of your data!  Are you sure?");
-    	builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() 
-    	{
-			
-			@Override
-			public void onClick(DialogInterface dialog, int which) 
-			{
-				mDB.Remove();
-				RefreshView();
-			}
-		});
-    	builder.setNegativeButton("No", new DialogInterface.OnClickListener() 
-    	{
-			
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				// Nothing
-			}
-		});
-    	builder.show();
-   }
-    
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("WARNING!");
+        builder.setMessage("This will delete all of your data!  Are you sure?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() 
+        {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) 
+            {
+                mDB.Remove();
+                RefreshView();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() 
+        {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Nothing
+            }
+        });
+        builder.show();
+    }
+
     private void LoadAstrid()
     {
-    	// Look for a zip file in the proper location
-    	String loc = Environment.getExternalStorageDirectory() + "/AstridImport/";
-    	AlertDialog.Builder errorBuilder = new AlertDialog.Builder(this);
-    	errorBuilder.setTitle("Error reading Astrid Import!");
-    	ZipInputStream zipStream = null;
-		InputStream stream = null;
-    	try
-    	{
-    		// Try to safely open the file
-    		String errText = "Cannot find Astrid file in " + loc;
-    		File astridDir = new File(loc);
-    		if (!astridDir.isDirectory())
-    		{
-    			throw new Exception(errText);
-    		}
-    		
-    		File[] files = astridDir.listFiles();
-    		if (files.length != 1)
-    		{
-    			throw new Exception(errText);
-    		}
-    		
-    		File astridFile = files[0];
-    		if (!astridFile.isFile())
-    		{
-    			throw new Exception(errText);
-    		}
-    		
-    		// Try to unzip the file and unpack the tasks
-    		errText = "Could not unzip file " + astridFile.getAbsolutePath();
-    		stream = new FileInputStream(astridFile);
-    		zipStream = new ZipInputStream(stream);
-    		
-    		ZipEntry tasksEntry = null;
-    		while ((tasksEntry = zipStream.getNextEntry()) != null)
-    		{
-    			if (tasksEntry.getName().equals("tasks.csv"))
-    			{
-    				break;
-    			}
-    		}
-    		if (tasksEntry == null)
-    		{
-    			throw new Exception(errText);
-    		}
-    		
-    		int size = (int)tasksEntry.getSize();
-    		byte tasksContent[] = new byte[size];
-    		int offset = 0;
-    		while (size != 0)
-    		{
-    			int read = zipStream.read(tasksContent, offset, size);
-    			if (read == 0)
-    			{
-    				throw new Exception(errText);
-    			}
-    			offset += read;
-    			size -= read;
-    		}
-    		
-    		String tasksString = new String(tasksContent, "UTF-8");
-    		
-    		// Parse the tasks in the task list
-    		errText = "Could not parse task list";
-    		List<String> taskLines = new LinkedList<String>(Arrays.asList(tasksString.split("\n")));
-    		// Remove the header row
-    		taskLines.remove(0);
-    		// Some tasks have newlines in quotes, so we have to adjust for that.
-    		ListIterator<String> it = taskLines.listIterator();
-    		while (it.hasNext())
-    		{
-    			String task = it.next();
-    			while (CountQuotes(task) % 2 == 1)
-    			{
-    				task += it.next();
-    				it.remove();
-    			}
-    		} 
-     		List<String> tasks = new LinkedList<String>();
-    		for (String taskLine : taskLines) {
-				String taskFields[] = taskLine.split(",");
-				tasks.add(taskFields[0]);
-				mDB.AddTask(taskFields[0]);
-			}
-    		
-			RefreshView();
-    	}
-    	catch (Exception e)
-    	{
-    		AlertDialog dlg = errorBuilder.setMessage(e.getMessage()).create();
-    		dlg.show();
-    	}
-    	finally
-    	{
-	    	
-			try
-			{
-		    	if (zipStream != null)
-		    	{
-	    			zipStream.close();
-	    		}
-		    	if (stream != null)
-		    	{
-		    		stream.close();
-		    	}
-	    	}
-	    	catch (Exception e)
-	    	{
-	    		AlertDialog dlg = errorBuilder.setMessage(e.getMessage()).create();
-	    		dlg.show();
-	    	}
-    	}
+        // Look for a zip file in the proper location
+        String loc = Environment.getExternalStorageDirectory() + "/AstridImport/";
+        AlertDialog.Builder errorBuilder = new AlertDialog.Builder(this);
+        errorBuilder.setTitle("Error reading Astrid Import!");
+        ZipInputStream zipStream = null;
+        InputStream stream = null;
+        try
+        {
+            // Try to safely open the file
+            String errText = "Cannot find Astrid file in " + loc;
+            File astridDir = new File(loc);
+            if (!astridDir.isDirectory())
+            {
+                throw new Exception(errText);
+            }
+
+            File[] files = astridDir.listFiles();
+            if (files.length != 1)
+            {
+                throw new Exception(errText);
+            }
+
+            File astridFile = files[0];
+            if (!astridFile.isFile())
+            {
+                throw new Exception(errText);
+            }
+
+            // Try to unzip the file and unpack the tasks
+            errText = "Could not unzip file " + astridFile.getAbsolutePath();
+            stream = new FileInputStream(astridFile);
+            zipStream = new ZipInputStream(stream);
+
+            ZipEntry tasksEntry = null;
+            while ((tasksEntry = zipStream.getNextEntry()) != null)
+            {
+                if (tasksEntry.getName().equals("tasks.csv"))
+                {
+                    break;
+                }
+            }
+            if (tasksEntry == null)
+            {
+                throw new Exception(errText);
+            }
+
+            int size = (int)tasksEntry.getSize();
+            byte tasksContent[] = new byte[size];
+            int offset = 0;
+            while (size != 0)
+            {
+                int read = zipStream.read(tasksContent, offset, size);
+                if (read == 0)
+                {
+                    throw new Exception(errText);
+                }
+                offset += read;
+                size -= read;
+            }
+
+            String tasksString = new String(tasksContent, "UTF-8");
+
+            // Parse the tasks in the task list
+            errText = "Could not parse task list";
+            List<String> taskLines = new LinkedList<String>(Arrays.asList(tasksString.split("\n")));
+            // Remove the header row
+            taskLines.remove(0);
+            // Some tasks have newlines in quotes, so we have to adjust for that.
+            ListIterator<String> it = taskLines.listIterator();
+            while (it.hasNext())
+            {
+                String task = it.next();
+                while (CountQuotes(task) % 2 == 1)
+                {
+                    task += it.next();
+                    it.remove();
+                }
+            } 
+            List<String> tasks = new LinkedList<String>();
+            for (String taskLine : taskLines) {
+                String taskFields[] = taskLine.split(",");
+                tasks.add(taskFields[0]);
+                mDB.AddTask(taskFields[0]);
+            }
+
+            RefreshView();
+        }
+        catch (Exception e)
+        {
+            AlertDialog dlg = errorBuilder.setMessage(e.getMessage()).create();
+            dlg.show();
+        }
+        finally
+        {
+
+            try
+            {
+                if (zipStream != null)
+                {
+                    zipStream.close();
+                }
+                if (stream != null)
+                {
+                    stream.close();
+                }
+            }
+            catch (Exception e)
+            {
+                AlertDialog dlg = errorBuilder.setMessage(e.getMessage()).create();
+                dlg.show();
+            }
+        }
     }
-    
+
     static private int CountQuotes(String string)
     {
-    	int count = 0;
-    	for (int i = 0; i < string.length(); ++i) 
-    	{
-			switch (string.charAt(i))
-			{
-			case '\\':
-				++i;
-				break;
-			case '\"':
-				++count;
-				break;
-			default:
-				// Nothing
-			}
-		}
-    	return count;
+        int count = 0;
+        for (int i = 0; i < string.length(); ++i) 
+        {
+            switch (string.charAt(i))
+            {
+            case '\\':
+                ++i;
+                break;
+            case '\"':
+                ++count;
+                break;
+            default:
+                // Nothing
+            }
+        }
+        return count;
     }
-    
+
     private TaskDatabase mDB = new TaskDatabase(this);
     private CursorAdapter mAdapter = null;
 }
